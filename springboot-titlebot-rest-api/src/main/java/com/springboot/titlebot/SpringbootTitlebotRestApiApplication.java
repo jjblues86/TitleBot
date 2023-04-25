99fb4d62307cf30e923fecbaab086cbcf24c840d
Jerome Joof <jeromejoof@Jeromes-MacBook-Pro.local>
2023-04-25 13:14:59 -0700

    initial commit to setup project structure.

---
+++ springboot-titlebot-rest-api/src/main/java/com/springboot/titlebot/SpringbootTitlebotRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.titlebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTitlebotRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTitlebotRestApiApplication.class, args);
+	}
+}
